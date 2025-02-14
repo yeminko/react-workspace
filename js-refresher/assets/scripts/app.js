72053611ca2a070ba020ebac80814367ad77fc7c
yeminko <ye775mk@prestigein.com>
2025-02-14 07:54:58 +0630

    feat: add initial project structure with assets, configuration files, and basic components for React application

---
+++ js-refresher/assets/scripts/app.js
@@ -0,0 +1,11 @@
+import defaultExport, { apiKey, url as link } from "./util.js";
+import * as util from "./util.js";
+
+console.log(defaultExport);
+console.log(apiKey);
+console.log(link);
+
+// Same as above, but using the util object
+console.log(util.default);
+console.log(util.apiKey);
+console.log(util.url);
---
+++ js-refresher/assets/scripts/util.js
@@ -0,0 +1,3 @@
+export default "Hello, World!";
+export const apiKey = "1234567890";
+export const url = "https://example.com";
