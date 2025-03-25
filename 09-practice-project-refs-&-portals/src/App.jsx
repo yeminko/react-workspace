dd1600056f5d1185126b8cfc7d5b2ea2dadb2555
yeminko <ye775mk@prestigein.com>
2025-03-25 08:30:48 +0630

    feat: implement project management features; add NewProject and NoProjectSelected components, and enhance App and ProjectsSideBar for project handling

--- 09-practice-project-refs-&-portals/src/App.jsx
+++ 09-practice-project-refs-&-portals/src/App.jsx
@@ -1,11 +1,37 @@
-import NewProject from "./components/NewProject";
+import { useState } from "react";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSideBar from "./components/ProjectsSideBar";
+import NewProject from "./components/NewProject";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    project: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  function handleAddProject() {}
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSideBar />
-      <NewProject />
+      <ProjectsSideBar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
