4d23d654e6b41b26cd3e6aa733fe81055d802b28
yeminko <yeminko47@gmail.com>
2025-06-17 08:26:40 +0630

    feat: implement core components for food order app including Header, Meals, MealItem, and Button; add CartContext for state management

---
+++ 19-food-order-app/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import logoImg from "../assets/logo.jpg";
+import Button from "./UI/Button";
+
+export default function Header() {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logoImg} alt="ReactFood Logo" />
+        <h1>ReactFood</h1>
+      </div>
+
+      <nav>
+        <Button textOnly>Cart (0)</Button>
+      </nav>
+    </header>
+  );
+}
---
+++ 19-food-order-app/src/components/Meals.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import MealItem from "./MealItem";
+
+export default function Meals() {
+  const [loadedMeals, setLoadedMeals] = useState([]);
+
+  useEffect(() => {
+    async function fetchMeals() {
+      const response = await fetch("http://localhost:3000/meals");
+
+      if (!response.ok) {
+        // Handle the error appropriately
+      }
+
+      const meals = await response.json();
+      setLoadedMeals(meals);
+    }
+    fetchMeals();
+  }, []);
+
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <MealItem key={meal.id} meal={meal} />
+      ))}
+    </ul>
+  );
+}
