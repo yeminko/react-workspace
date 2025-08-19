15cef6b3a4a7998358044ba6939700a4062ad18f
yeminko <yeminko47@gmail.com>
2025-08-19 11:16:30 +0630

    feat: enhance event fetching logic; add max parameter support and improve query handling

--- 25-tanstack-query-app/src/components/Events/EditEvent.jsx
+++ 25-tanstack-query-app/src/components/Events/EditEvent.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { fetchEvent, updateEvent } from "../../util/http.js";
+import { fetchEvent, queryClient, updateEvent } from "../../util/http.js";
 import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
@@ -18,6 +18,26 @@ export default function EditEvent() {
 
   const { mutate } = useMutation({
     mutationFn: updateEvent,
+    onMutate: async (data) => {
+      const newEvent = data.event;
+
+      // To prevent query conflict
+      await queryClient.cancelQueries({ queryKey: ["events", params.id] });
+      const previousEvent = queryClient.getQueryData(["events", params.id]);
+
+      queryClient.setQueryData(["events", params.id], newEvent);
+
+      return { previousEvent };
+    },
+
+    onError: (error, variables, context) => {
+      queryClient.setQueryData(["events", params.id], context.previousEvent);
+    },
+
+    // To ensure the query is fresh
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["events", params.id] });
+    },
   });
 
   function handleSubmit(formData) {
@@ -69,4 +89,11 @@ export default function EditEvent() {
   }
 
   return <Modal onClose={handleClose}>{content}</Modal>;
+}
+
+export function loader() {
+  queryClient.fetchQuery({
+    queryKey: 
+    queryFn: 
+  })
 }
