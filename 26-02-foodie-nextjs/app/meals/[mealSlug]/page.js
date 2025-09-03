90fca4964a62947a862aeeb31e38905b2ed4a626
yeminko <yeminko47@gmail.com>
2025-09-03 09:31:33 +0630

    feat: implement loading, error, and not found pages; enhance Meals page with suspense and loading states; add styles for loading animations

--- 26-02-foodie-nextjs/app/meals/[mealSlug]/page.js
+++ 26-02-foodie-nextjs/app/meals/[mealSlug]/page.js
@@ -1,5 +1,24 @@
+import Image from "next/image";
+import classes from "./page.module.css";
+
 export default function MealDetailsPage({ params }) {
   const { mealSlug } = params;
 
-  return <h1>Meal Detail Page for {mealSlug}</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>Meal Title</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${"EMAIL"}`}>Name</a>
+          </p>
+          <p className={classes.summary}></p>
+        </div>
+      </header>
+      <main className={classes.main}></main>
+    </>
+  );
 }
---
+++ 26-02-foodie-nextjs/app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Page Not Found</h1>
+      <p>Sorry, we could not find the page you were looking for.</p>
+    </main>
+  );
+}
