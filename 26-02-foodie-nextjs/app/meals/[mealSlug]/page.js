0dc1c3e77800681bbe7acf55d6f9814e0b8b4007
yeminko <yeminko47@gmail.com>
2025-09-04 09:21:13 +0630

    feat: implement MealDetails and ShareMeal pages; add NotFound component; enhance image handling with ImagePicker; style updates for improved layout and user experience

--- 26-02-foodie-nextjs/app/meals/[mealSlug]/page.js
+++ 26-02-foodie-nextjs/app/meals/[mealSlug]/page.js
@@ -1,24 +1,39 @@
 import Image from "next/image";
 import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
 export default function MealDetailsPage({ params }) {
-  const { mealSlug } = params;
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
 
   return (
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image fill />
+          <Image fill src={meal.image} alt={meal.title} />
         </div>
         <div className={classes.headerText}>
-          <h1>Meal Title</h1>
+          <h1>{meal.title}</h1>
           <p className={classes.creator}>
-            by <a href={`mailto:${"EMAIL"}`}>Name</a>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
           </p>
-          <p className={classes.summary}></p>
+          <p className={classes.summary}>{meal.summary}</p>
         </div>
       </header>
-      <main className={classes.main}></main>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
     </>
   );
 }
---
+++ 26-02-foodie-nextjs/app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Meal Not Found</h1>
+      <p>Sorry, we could not find the meal you were looking for.</p>
+    </main>
+  );
+}
