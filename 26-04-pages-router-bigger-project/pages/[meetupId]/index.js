f462fffb1ba594d0adfc67922ed51a4dc32e4911
yeminko <yeminko47@gmail.com>
2025-09-25 11:13:17 +0630

    feat: implement MeetupDetail component and integrate it into the application

---
+++ 26-04-pages-router-bigger-project/pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+export default function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg"
+      title="A First Meetup"
+      address="Some Street 5, Some City"
+      description="This is a first meetup"
+    />
+  );
+}
