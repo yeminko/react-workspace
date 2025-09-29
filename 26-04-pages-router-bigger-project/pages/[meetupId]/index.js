80fb704ccff6300a407bb2bfbaacda8dbac4b130
yeminko <yeminko47@gmail.com>
2025-09-29 09:18:46 +0630

    feat: add API endpoint for creating new meetups

--- 26-04-pages-router-bigger-project/pages/[meetupId]/index.js
+++ 26-04-pages-router-bigger-project/pages/[meetupId]/index.js
@@ -10,3 +10,38 @@ export default function MeetupDetails() {
     />
   );
 }
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "A First Meetup",
+        address: "Some Street 5, Some City",
+        description: "This is a first meetup",
+      },
+    },
+  };
+}
---
+++ 26-04-pages-router-bigger-project/pages/api/new-meetup.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const { title, image, address, description } = data;
+  }
+}
