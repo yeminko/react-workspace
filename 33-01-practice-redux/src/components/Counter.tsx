c20208810e2c56741074c892ea3c8712ea3b11fe
yeminko <yeminko47@gmail.com>
2025-12-16 20:00:30 +0630

    feat: add Counter component with increment, decrement, and custom increment functionality; include CSS module for styling

--- 33-01-practice-redux/src/components/Counter.tsx
+++ 33-01-practice-redux/src/components/Counter.tsx
@@ -1,12 +1,44 @@
-import { useAppSelector } from "../hooks/useStore";
+import { useState } from "react";
+import { useAppDispatch, useAppSelector } from "../hooks/useStore";
+import { decrement, increment, incrementByAmount } from "../store/counterSlice";
+import classes from "./Counter.module.css";
 
 export default function Counter() {
   const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+  const [input, setInput] = useState("");
+
+  function handleIncrement() {
+    dispatch(increment());
+  }
+
+  function handleDecrement() {
+    dispatch(decrement());
+  }
+
+  function handleCustomIncrement() {
+    dispatch(incrementByAmount(Number(input) || 0));
+  }
 
   return (
-    <>
+    <section>
       <h1>My Counter</h1>
       <h2>Current Count: {count}</h2>
-    </>
+      <button onClick={handleDecrement} className={classes["m-right"]}>
+        Decrement
+      </button>
+      <button onClick={handleIncrement}>Increment</button>
+
+      <p>
+        <input
+          placeholder="Enter custom increment"
+          className={classes["m-right"]}
+          type="number"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={handleCustomIncrement}>Custom Increment</button>
+      </p>
+    </section>
   );
 }
