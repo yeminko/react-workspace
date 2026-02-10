65009181026ca42abad1bce22fdf6b3441fac9ff
yeminko <yeminko47@gmail.com>
2026-02-10 16:07:50 +0630

    feat: refactor App component structure; add Header and AnotherHeader components with styles

---
+++ medium/01-simple-app/src/components/AnotherHeader.tsx
@@ -0,0 +1,5 @@
+import "./AnotherHeader.css";
+
+export default function AnotherHeader() {
+  return <h1 className="header">Another Header with blue color</h1>;
+}
---
+++ medium/01-simple-app/src/components/Header.tsx
@@ -0,0 +1,5 @@
+import "./Header.css";
+
+export default function Header() {
+  return <h1 className="header">Header with red color</h1>;
+}
