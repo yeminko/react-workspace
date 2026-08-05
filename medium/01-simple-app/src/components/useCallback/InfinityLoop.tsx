123cbe5da8c091ca12ec89e8df3fcb0fbb29fb3f
yeminko <yeminko47@gmail.com>
2026-08-05 22:32:02 +0630

    feat: refactor InfinityLoop component to use useCallback for prettyPrint function

--- medium/01-simple-app/src/components/useCallback/InfinityLoop.tsx
+++ medium/01-simple-app/src/components/useCallback/InfinityLoop.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 export function InfinityLoop() {
   const [count, setCount] = useState(0);
 
-  function prettyPrint() {
+  const prettyPrint = useCallback(() => {
     console.log("Pretty Print");
-  }
+  }, []); // No dependencies, so the function reference will remain the same
 
   useEffect(() => {
     prettyPrint();
